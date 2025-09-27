18cf6b622e1845688d5dd5fb82bcf564a812ec87
rajiv <rajiv1405.r@gmail.com>
2025-09-28 03:58:23 +0530

    message

--- 08-Specialvaribales.sh
+++ 08-Specialvaribales.sh
@@ -2,3 +2,7 @@
 
 echo "All variables/arguments are passed to the script:  $@"
 echo "All variables/arguments are passed to the script:  $*"
+echo "scriptname :$0"
+echo "currect working directory :$PWD"
+echo "howm directory: $HOME"
+echo "who is running :$USER"
