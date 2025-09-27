4b051853a53cab8af851e4cb5438e377bfdda708
rajiv <rajiv1405.r@gmail.com>
2025-09-28 04:38:25 +0530

    message

---
+++ 09-datatype.sh
@@ -0,0 +1,12 @@
+#! /sbin/bash
+
+NUMBER1=100
+NUMBER2=100
+
+SUM=$(($NUMBER1+$NUMBER2))
+
+echo "total SUM is : $SUM"
+
+LEADERS=("modi", "lincon", "bushh")
+
+echo "Number os leaders are ${LEADERS[@]}"
