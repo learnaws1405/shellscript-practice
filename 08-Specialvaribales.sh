36d29a7ec05f2c9d8e34aff657f305d4218a89b8
rajiv <rajiv1405.r@gmail.com>
2025-09-28 04:00:21 +0530

    message

--- 08-Specialvaribales.sh
+++ 08-Specialvaribales.sh
@@ -6,3 +6,6 @@ echo "scriptname :$0"
 echo "currect working directory :$PWD"
 echo "howm directory: $HOME"
 echo "who is running :$USER"
+echo "PID of the running script :$$"
+sleep &
+echo "PID of the last command wich is running in backround :$!"
